1808a0ec02a62efc22cee1861aa681159e411cb0
ssklykov <ssklykov@yandex.ru>
2019-08-27 19:35:07 +0200

    Playing around basic functionality of ImageJ (again)

---
+++ src/main/java/PluginTraining.java
@@ -0,0 +1,14 @@
+
+
+
+/**
+ * Training a few basic operations within plugin context
+ */
+
+
+public class PluginTraining {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,11 +1,30 @@
-import ij.ImageJ;
+import net.imagej.ImageJ;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+
+import java.io.IOException;
 
 public class Test2 {
 
     public static void main(String[] args) {
 
         ImageJ ij1 = new ImageJ();
-        String info = ij1.getInfo();
-        System.out.println(info);
+        ij1.ui().showUI();
+        Img image; // most generic image container
+        IterableInterval image2;
+        RandomAccessibleInterval image3; RandomAccessibleInterval gaussImg;
+        try {
+            image3 = (RandomAccessibleInterval) ij1.io().open("src/main/resources/lena.jpg");
+            image2 = (IterableInterval) image3;
+            ij1.op().convert().uint8(image2); // the conversion operation demands other type of image
+            ij1.ui().show(image2);
+            gaussImg =  ij1.op().filter().gauss(image3,2.0);
+            ij1.ui().show(gaussImg);
+
+        } catch (IOException e) {
+            System.out.println("Problem with file opening");
+        }
+
     }
 }
