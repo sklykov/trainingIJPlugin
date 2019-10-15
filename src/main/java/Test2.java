2394319358eca735a5647ff6546ea86700169b67
ssklykov <ssklykov@yandex.ru>
2019-10-15 23:35:36 +0200

    The source img changed. TODO list - tasks

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -10,18 +10,22 @@ public class Test2 {
 
     public static void main(String[] args) {
 
-        ImageJ ij1 = new ImageJ();
-        ij1.ui().showUI();
+        ImageJ ij = new ImageJ();
+        ij.ui().showUI();
         Img image; // most generic image container
-        IterableInterval image2;
+        IterableInterval image2, saltPepImg;
         RandomAccessibleInterval image3; RandomAccessibleInterval gaussImg;
         try {
-            image3 = (RandomAccessibleInterval) ij1.io().open("src/main/resources/lena.jpg");
+            image3 = (RandomAccessibleInterval) ij.io().open("src/main/resources/nesvizh.jpg");
             image2 = (IterableInterval) image3;
-            ij1.op().convert().uint8(image2); // the conversion operation demands other type of image
-            ij1.ui().show(image2);
-            gaussImg =  ij1.op().filter().gauss(image3,2.0);
-            ij1.ui().show(gaussImg);
+            ij.op().convert().uint8(image2); // the conversion operation demands other type of image
+            /* TODO: 1) Why converted image from RGB to uint8 still represented as RGB?
+            *   2) Salt and pepper noise adding; 3) Old algorithm = denoising with edge preserving*/
+            ij.ui().show(image2);
+            gaussImg =  ij.op().filter().gauss(image3,2.0);
+            ij.ui().show(gaussImg);
+            // First complication - adding PoissonNoise
+            // saltPepImg = ij.op().filter().addPoissonNoise(null,image2);
 
         } catch (IOException e) {
             System.out.println("Problem with file opening");
